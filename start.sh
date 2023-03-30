de8d5909c3a43fa618888a6edf79d81a0ada50d5
RexChien <121141653+RexChien@users.noreply.github.com>
2023-03-30 16:52:30 +0800

    add default application

--- start.sh
+++ start.sh
@@ -2,19 +2,34 @@
 cd ~
 
 # edge (add apt source)
-echo "deb [arch=amd64] http://packages.microsoft.com/repos/edge/ stable main" | 
-  sudo tee "/etc/apt/sources.list.d/microsoft-edge-dev.list"
+echo "deb [arch=amd64] http://packages.microsoft.com/repos/edge/ stable main" \
+  | sudo tee "/etc/apt/sources.list.d/microsoft-edge-dev.list"
 
 # firefox developer edition (direct download)
 curl -L "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=zh-TW" | tar -xj
 echo "[Desktop Entry]
 Name=Firefox Developer Edition
+Type=Application
 Exec=$HOME/firefox/firefox
 Icon=$HOME/firefox/browser/chrome/icons/default/default128.png
-Categories=Network;" | sudo tee "/usr/share/applications/firefox-developer-edition.desktop"
+Categories=Network;" > .local/share/applications/firefox-developer-edition.desktop
 
 sudo apt update
-sudo apt install -y code microsoft-edge-dev google-chrome-stable gnome-keyring flameshot fonts-noto
+sudo apt install -y code microsoft-edge-dev google-chrome-stable gnome-keyring flameshot fonts-noto git
 
+# config git
 git config --global user.name "inMyHeart"
 git config --global user.email "inMyHeart@sample.com"
+
+
+# set firefox as default application
+mkdir -p .local/share/xfce4/helpers
+echo "[Desktop Entry]
+Name=Firefox Developer Edition
+Type=X-XFCE-Helper
+Icon=$HOME/firefox/browser/chrome/icons/default/default128.png
+X-XFCE-Category=WebBrowser
+X-XFCE-Commands=$HOME/firefox/firefox
+X-XFCE-CommandsWithParameter=$HOME/firefox/firefox "%s"
+" > .local/share/xfce4/helpers/firefox-developer-edition.desktop
+echo "WebBrowser=firefox-developer-edition" > .config/xfce4/helpers.rc
