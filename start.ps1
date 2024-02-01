19b28ace8674b8e9c33376982ab2abc798f5c7cf
RexChien <121141653+RexChien@users.noreply.github.com>
2024-02-01 13:11:24 +0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,16 +1,18 @@
 # winget
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx" -Name 'AllowDeploymentInSpecialProfiles' -Value 1 -PropertyType DWORD -Force | Out-Null
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+#New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx" -Name 'AllowDeploymentInSpecialProfiles' -Value 1 -PropertyType DWORD -Force | Out-Null
+#$ProgressPreference = 'SilentlyContinue'
+#Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+#Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+#Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-if ($ResolveWingetPath){ $WingetPath = $ResolveWingetPath[-1].Path }
-$ENV:PATH += ";$WingetPath"
+#Get-ChildItem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*" -Directory | Where-Object { $_.Name -like "*_x64_*" } | foreach  {$folderPath = $_.FullName; TAKEOWN /F $folderPath /R /A /D Y; ICACLS $folderPath /grant Administrators:F /T}
+
+#$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+#if ($ResolveWingetPath){ $WingetPath = $ResolveWingetPath[-1].Path }
+#$ENV:PATH += ";$WingetPath"
 
 # chocolate
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
@@ -18,3 +20,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install -y -f firefox microsoft-edge-insider-dev dotnet-6.0-sdk dotnet-8.0-sdk dotnetcore-3.1-sdk vscode git visualstudio-installer powershell-core
+
+# config git
+git config --global user.name "inMyHeart"
+git config --global user.email "inMyHeart@sample.com"
