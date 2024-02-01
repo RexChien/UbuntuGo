eeb79fab00ceeefe901d9a016d9b5356c1b2f245
RexChien <121141653+RexChien@users.noreply.github.com>
2024-02-01 13:14:23 +0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -19,7 +19,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install -y -f firefox microsoft-edge-insider-dev dotnet-6.0-sdk dotnet-8.0-sdk dotnetcore-3.1-sdk vscode git visualstudio-installer powershell-core
+choco install -y -f firefox microsoft-edge-insider-dev dotnet-6.0-sdk dotnet-8.0-sdk dotnetcore-3.1-sdk vscode git powershell-core
 
 # config git
 git config --global user.name "inMyHeart"
